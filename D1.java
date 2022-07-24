262a5bce811fb7035faf8d0a5f26a2b0f4fd4370
Dhanush4-hash <109908326+Dhanush4-hash@users.noreply.github.com>
2022-07-24 19:26:11 +0530

    Add files via upload

---
+++ D1.java
@@ -0,0 +1,14 @@
+package DJASS;
+
+import java.util.Date;
+
+public class D1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Date d1=new Date();
+		System.out.println(d1);
+
+	}
+
+}
